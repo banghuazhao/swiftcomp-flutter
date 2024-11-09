9922a94c6e635a138181c9df7134194759480ea4
Lulin Yang <iday4633@gmail.com>
2024-11-10 11:01:01 +1300

    signup optimize

--- domain/lib/repositories_abstract/auth_repository.dart
+++ domain/lib/repositories_abstract/auth_repository.dart
@@ -3,9 +3,10 @@
 import '../entities/user.dart';
 
 abstract class AuthRepository {
-  Future<User> signup(String email, String password, {String? name});
+  Future<User> signup(String email, String password, String verificationCode,{String? name});
   Future<String> login(String email, String password);
   Future<void> logout();
   Future<void> forgetPassword(String email);
   Future<String> resetPassword(String email, String newPassword, String confirmationCode);
+  Future<void> sendSignupVerificationCode(String email);
 }
--- lib/presentation/settings/viewModels/signup_view_model.dart
+++ lib/presentation/settings/viewModels/signup_view_model.dart
@@ -10,6 +10,8 @@ class SignupViewModel extends ChangeNotifier {
 
   bool obscureTextNewPassword = true;
   bool obscureTextConfirmPassword = true;
+  bool isSignUp = false;
+
 
   void toggleNewPasswordVisibility() {
     obscureTextNewPassword = !obscureTextNewPassword;
@@ -30,13 +32,13 @@ class SignupViewModel extends ChangeNotifier {
   String? _errorMessage;
   String? get errorMessage => _errorMessage;
 
-  Future<User?> signup(String email, String password, {String? name}) async {
+  Future<User?> signup(String email, String password,String verificationCode, {String? name}) async {
     _isLoading = true;
     _errorMessage = null;
     notifyListeners();
 
     try {
-      User user = await authUseCase.signup(email, password, name: name);
+      User user = await authUseCase.signup(email, password, verificationCode, name: name);
       return user;
     } catch (e) {
       _errorMessage = 'Signup failed: ${e.toString()}';
@@ -45,5 +47,34 @@ class SignupViewModel extends ChangeNotifier {
       _isLoading = false;
       notifyListeners();
     }
+  }
+
+  Future<void> continueSignUp(String email) async {
+    _setLoadingState(true);
+    _errorMessage = '';
+
+    try {
+      // Call the auth use case to send the confirmation code to the email
+      await authUseCase.sendSignupVerificationCode(email);
+      isSignUp = true;
+    } catch (error) {
+      final errorMessage = error.toString();
+      print("Error Message: $errorMessage");
+
+      if (errorMessage.contains("already registered")) {
+        _errorMessage = 'Email is already registered';
+      } else if (errorMessage.contains("Invalid email")) {
+        _errorMessage = 'Invalid email address';
+      } else {
+        _errorMessage = 'Failed to send verification code.';
+      }
+    } finally {
+      _setLoadingState(false);
+    }
+  }
+
+  void _setLoadingState(bool value) {
+    _isLoading = value;
+    notifyListeners();
   }
 }
