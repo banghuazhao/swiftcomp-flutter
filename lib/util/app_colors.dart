2e3137a9811121a43ba9079d0e923b560a1da5cd
Banghua Zhao <banghua.zhao@gmail.com>
2025-03-01 23:11:51 +1300

    Add AppColors

---
+++ lib/util/app_colors.dart
@@ -0,0 +1,11 @@
+// app_colors.dart
+// import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color.fromRGBO(66, 66, 66, 1.0);
+  static Color secondary = Color.fromRGBO(66, 66, 66, 1.0);
+  static Color background = Colors.grey.shade100;
+// Add more colors as needed
+}
